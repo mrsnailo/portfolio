8e4b51a6ecfd6fb7d4c259691ca8f505e371e206
mrsnailo <shahidparvez.official@gmail.com>
2025-09-17 07:39:03 +0600

    feat: Added mobile navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
 
 interface NavLink {
   name: string;
@@ -9,29 +12,54 @@ interface NavLink {
 type NavLinks = NavLink[];
 
 const navLinks: NavLinks = [
-  { name: "Home", href: "/", isActive: true },
-  { name: "Skills", href: "/about" },
-  { name: "Project", href: "/contact" },
-  { name: "Experience", href: "/contact" },
-  { name: "Contact", href: "/contact" },
+  { name: "Home", href: "#home", isActive: true },
+  { name: "Skills", href: "#about" },
+  { name: "Project", href: "#contact" },
+  { name: "Experience", href: "#contact" },
+  { name: "Contact", href: "#contact" },
 ];
 
 const Header: React.FC = () => {
+  const [menuVisibility, setMenuVisibility] = useState(false);
   return (
-    <header className="border-b py-5">
-      <div className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-2xl font-bold gradient-text">Shahid Parvez</h1>
-        <nav>
-          <ul className="flex space-x-6">
+    <>
+      <header className="py-5">
+        <div className="container mx-auto flex justify-between items-center px-4">
+          <h1 className="text-2xl font-bold gradient-text">Shahid Parvez</h1>
+          {/* main menu */}
+          <nav>
+            <ul className="nav-links">
+              {navLinks.map((link) => (
+                <li key={link.name}>
+                  <a href={link.href}>{link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          {/* Mobile menu button */}
+          <button
+            className="mobile-menu-btn"
+            onClick={() => setMenuVisibility(!menuVisibility)}
+          >
+            {menuVisibility ? <X /> : <Menu />}
+          </button>
+        </div>
+      </header>
+
+      {/* mobile menu */}
+      {menuVisibility && (
+        <div className="mobile-menu">
+          <ul>
             {navLinks.map((link) => (
-              <li className="nav-links" key={link.name}>
+              <li key={link.name}>
+                {" "}
                 <a href={link.href}>{link.name}</a>
               </li>
             ))}
           </ul>
-        </nav>
-      </div>
-    </header>
+        </div>
+      )}
+    </>
   );
 };
 
